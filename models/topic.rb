e946816a083c93faa6ef8a5b78fd097467658cd8
Quinn Shanahan <q.shanahan@gmail.com>
2014-04-19 15:57:46 -0500

    ascending.

--- models/topic.rb
+++ models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ActiveRecord::Base
   belongs_to :section
   has_many :activities
 
-  default_scope -> { order('topics.name desc') }
+  default_scope -> { order(:name) }
 
   def section
     return NilSection if super.nil?
