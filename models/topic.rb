2c1bf522004e8b7043c7824adc4c28c748097deb
Quinn Shanahan <q.shanahan@gmail.com>
2014-04-19 15:45:52 -0500

    Add default scope

--- models/topic.rb
+++ models/topic.rb
@@ -2,6 +2,8 @@ class Topic < ActiveRecord::Base
   belongs_to :section
   has_many :activities
 
+  default_scope -> { order('topics.name desc') }
+
   def section
     return NilSection if super.nil?
     super
