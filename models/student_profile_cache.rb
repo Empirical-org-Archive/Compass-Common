1cf99ebe2f143ebc2674ea6ea0d9f44966bc0569
Quinn Shanahan <q.shanahan@gmail.com>
2014-05-19 13:52:25 -0400

    array-ize

--- models/student_profile_cache.rb
+++ models/student_profile_cache.rb
@@ -1,6 +1,6 @@
 module StudentProfileCache
   def invalidate students
-    students.each do |student|
+    Array.wrap(students).each do |student|
       ctrl_key = ActiveSupport::Cache.expand_cache_key('student-profile-vars-'+ student.id.to_s, :controller)
       view_key = ApplicationController.new.fragment_cache_key('student-profile-'+ student.id.to_s)
 
