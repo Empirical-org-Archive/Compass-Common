22ff9b4832bd86ccafdcb33d0b1652c6597718bc
Quinn Shanahan <q.shanahan@gmail.com>
2014-05-19 13:35:39 -0400

    Update profile cache

--- models/student_profile_cache.rb
+++ models/student_profile_cache.rb
@@ -4,8 +4,11 @@ module StudentProfileCache
       ctrl_key = ActiveSupport::Cache.expand_cache_key('student-profile-vars-'+ student.id.to_s, :controller)
       view_key = ApplicationController.new.fragment_cache_key('student-profile-'+ student.id.to_s)
 
-      Rails.cache.delete(ctrl_key)
-      Rails.cache.delete(view_key)
+      begin
+        Rails.cache.delete(ctrl_key)
+        Rails.cache.delete(view_key)
+      rescue Rest::HttpError
+      end
     end
   end
 
